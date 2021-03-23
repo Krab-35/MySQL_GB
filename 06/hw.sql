bf3bb71a848eb222d20969ee6ae785d37becad81
Krab-35 <k1r1ll@yandex.ru>
2021-03-23 18:48:46 +0300

    home work MySQL 06

---
+++ 06/hw.sql
@@ -0,0 +1,58 @@
+-- 1 задание.
+-- Упростил бы запрос в таком вид (подключил три таблицы и в команде на проверку условия, задавал бы только один id):
+
+SELECT
+	lastname,
+	firstname, 
+	p.hometown,
+	m.filename 
+FROM users u, profiles p, media m
+WHERE
+	u.id = 1 AND p.user_id = u.id AND m.user_id = u.id
+;
+
+-- 2 задание.
+-- для пользователя с id=9 код выдает человека, находящийся в друзьях 9,
+-- который больше всех оправлял сообщений пользователю с id=9
+
+SELECT 
+	firstname,
+	lastname
+FROM
+	users
+WHERE
+	id = (
+		SELECT 
+			from_user_id
+		FROM messages
+		WHERE
+			from_user_id IN (
+				SELECT initiator_user_id 
+				FROM friend_requests
+				WHERE
+					target_user_id = 9 AND status = 'approved'	
+			)
+		GROUP BY from_user_id
+		ORDER BY COUNT(from_user_id) DESC
+		LIMIT 1
+	)
+;
+
+-- задание 3
+
+SELECT user_id,
+	id
+FROM media
+WHERE
+	user_id IN (
+		SELECT user_id
+		FROM profiles
+		ORDER BY TIMESTAMPDIFF(YEAR, birthday, NOW())
+		LIMIT 10
+		)
+
+
+
+
+	
+
